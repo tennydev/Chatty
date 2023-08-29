1aaf561d411719aace9e0fc88a2da8725db4def1
Jason Nguyen <128450487+tennydev@users.noreply.github.com>
2023-08-28 21:29:04 -0400

    Untracked changes, will figure out

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images:{
+    domains: [
+      'res.cloudinary.com', 
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+      'platform-lookaside.fbsbx.com',
+    ]
+  }
+}
 
 module.exports = nextConfig
